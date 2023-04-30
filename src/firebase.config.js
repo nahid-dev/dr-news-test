8a4b5a679f7356ee0bca26ba5eec946f8f32c2f3
nahid-dev <nahidv232@gmail.com>
2023-04-30 13:36:46 +0600

    environment variable added

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDLjE4y8rz1gEQj2NHfHeRwfVjCQeYw5Hk",
-  authDomain: "dr-news-test.firebaseapp.com",
-  projectId: "dr-news-test",
-  storageBucket: "dr-news-test.appspot.com",
-  messagingSenderId: "974303570778",
-  appId: "1:974303570778:web:068c4ba8dbf892b1412163",
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
